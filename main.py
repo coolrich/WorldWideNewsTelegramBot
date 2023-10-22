b47d48bb18796d3e62b888e9f8ac90a7843bfac2
CraftBox <i.domnich2017@gmail.com>
2023-10-22 12:59:49 +0300

    - Developing of the MVC pattern. - Created a class BotView

--- main.py
+++ main.py
@@ -28,6 +28,25 @@ class BotModel:
         self.world_news_dict = None
 
 
+class BotView:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_news_info(news_dict, news_deque, title):
+        article_info = news_dict[title]
+        text = article_info[0]
+        url = article_info[1]
+        txt_len = len(text)
+        notification = f"{'-' * txt_len}\nНовини про:\nЗаголовок: {title}\nТекст: {text}\nUrl: {url}!\n{'-' * txt_len}"
+        title = f'*{escape_markdown(title)}*'
+        text = escape_markdown(text)
+        url = f'[Посилання на статтю]({url})'
+        post = f'{title}\n\n{text}\n\n{url}'
+        news_deque.popleft()
+        return post
+
+
 class BotController:
     def __init__(self, a_news_manager, a_lock):
         self.bot_model = BotModel(a_news_manager, a_lock)
@@ -44,19 +63,6 @@ class BotController:
                 self.bot_model.bot.reply_to(message, "Привіт, я бот для Telegram, який показує новини",
                                             reply_markup=self.bot_model.markup)
 
-            def get_news_info(news_dict, news_deque, title):
-                article_info = news_dict[title]
-                text = article_info[0]
-                url = article_info[1]
-                txt_len = len(text)
-                notification = f"{'-' * txt_len}\nНовини про:\nЗаголовок: {title}\nТекст: {text}\nUrl: {url}!\n{'-' * txt_len}"
-                title = f'*{escape_markdown(title)}*'
-                text = escape_markdown(text)
-                url = f'[Посилання на статтю]({url})'
-                post = f'{title}\n\n{text}\n\n{url}'
-                news_deque.popleft()
-                return post
-
             @self.bot_model.bot.message_handler(
                 func=lambda message: message.text in ['Новини України', 'Новини Світу'])
             def send_news(message):
@@ -72,7 +78,7 @@ class BotController:
                 news_lang = 'ua' if news_type == 'Новини України' else 'en'
                 news_deque = get_news_deqs(chat_id)[news_lang]
 
-                while not news_deque:
+                if not news_deque:
                     self.bot_model.bot.send_message(chat_id=chat_id,
                                                     text=f'Більше новин BBC {news_lang} немає\!\n Починаємо з початку:',
                                                     parse_mode="MarkdownV2")
@@ -83,15 +89,13 @@ class BotController:
 
                 title = news_deque[0] if news_deque else None
                 news_dict = self.bot_model.ua_news_dict if news_lang == 'ua' else self.bot_model.world_news_dict
-                post = get_news_info(news_dict, news_deque, title)
+                post = BotView.get_news_info(news_dict, news_deque, title)
                 self.bot_model.bot.send_message(chat_id=chat_id, text=post, parse_mode="MarkdownV2")
-
-
 
             @self.bot_model.bot.message_handler(func=lambda message: True)
             def default_handler(message):
                 self.bot_model.bot.reply_to(message, "Я не розумію, що ви хочете сказати. Використовуйте меню "
-                                                          "нижче:",
+                                                     "нижче:",
                                             reply_markup=self.bot_model.markup)
 
             print("Bot manager has been starting...")
@@ -134,6 +138,7 @@ class NewsManager:
                 print("-" * 50)
             print(f"Sleeping in get_ua_news on {delay}...")
             time.sleep(delay)
+
 
 class ErrorHandler:
     @staticmethod
