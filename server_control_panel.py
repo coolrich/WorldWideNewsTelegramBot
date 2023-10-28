b9f8e7d2e6d297f753e166697d4e9aa8db12f1d9
CraftBox <i.domnich2017@gmail.com>
2023-10-28 10:25:14 +0300

    — In file server_control_panel.py an authorization mechanism has been added. — login.html file has been added.

--- server_control_panel.py
+++ server_control_panel.py
@@ -1,10 +1,14 @@
 import os
 import signal
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
 from controllers.application_controller import ApplicationController
 
-# TODO: Add authentication with login and password
+# TODO: Add authorization with login and password
 
 # Змінна-флаг для вказування на стан бота (активний або призупинений)
 bot_active = False
@@ -13,9 +17,36 @@ bot_active = False
 bot = ApplicationController()
 flask_app = Flask(__name__)
 
+flask_app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secure random key
+
+# Replace this with a database (e.g., SQLAlchemy) for production use.
+users = [{'username': 'admin', 'password': generate_password_hash('adminpassword')}]
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+@flask_app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = next((user for user in users if user['username'] == username), None)
+        if user and check_password_hash(user['password'], password):
+            # Successful login
+            flash('Login successful!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
 
 # Обробник головної сторінки
-@flask_app.route('/')
+@flask_app.route('/index')
 def index():
     return render_template('index.html')
 
