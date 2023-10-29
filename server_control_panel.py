a994c796127e6bf271cc1cb949e323c7c2956834
CraftBox <i.domnich2017@gmail.com>
2023-10-29 13:30:49 +0200

    — Login and logout functions for control panel have been implemented.

--- server_control_panel.py
+++ server_control_panel.py
@@ -1,5 +1,6 @@
 import os
 import signal
+import time
 
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_wtf import FlaskForm
@@ -8,7 +9,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 from controllers.application_controller import ApplicationController
-
+from flask_login import login_required, login_user, UserMixin, logout_user
+from flask_login import LoginManager
 
 # Змінна-флаг для вказування на стан бота (активний або призупинений)
 bot_active = False
@@ -18,9 +20,11 @@ bot = ApplicationController()
 flask_app = Flask(__name__)
 flask_app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a secure random key
 Bootstrap(flask_app)
+login_manager = LoginManager()
+login_manager.init_app(flask_app)
 
 # Replace this with a database (e.g., SQLAlchemy) for production use.
-users = [{'username': 'admin', 'password': generate_password_hash('adminpassword')}]
+__users = [{'username': 'admin', 'password': generate_password_hash('adminpassword')}]
 
 
 class LoginForm(FlaskForm):
@@ -29,15 +33,36 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 
+class User(UserMixin):
+    __user_counter = 0
+    __users = {}
+
+    def __init__(self):
+        self.id = str(User.__user_counter)
+        User.__user_counter = str(int(User.__user_counter) + 1)
+        User.__users[self.id] = self
+
+    @staticmethod
+    def get(user_id):
+        return User.__users.get(user_id, None)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
 @flask_app.route('/', methods=['GET', 'POST'])
-def login():
+def login_control_panel():
     form = LoginForm()
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        user = next((user for user in users if user['username'] == username), None)
+        user = next((user for user in __users if user['username'] == username), None)
         if user and check_password_hash(user['password'], password):
             # Successful login
+            user_obj = User()
+            login_user(user_obj)
             flash('Login successful!', 'success')
             return redirect(url_for('index'))
         else:
@@ -48,15 +73,16 @@ def login():
 # Обробник головної сторінки
 
 @flask_app.route('/index')
+@login_required
 def index():
     return render_template('index.html')
 
 
 # Обробник для команди запуску бота
 @flask_app.route('/start_bot')
+@login_required
 def start_bot():
     global bot_active
-
     if not bot_active:
         bot.start()
         bot_active = True
@@ -66,24 +92,29 @@ def start_bot():
 
 # Обробник для команди зупинення бота
 @flask_app.route('/stop_bot')
+@login_required
 def stop_bot():
     global bot_active
     if bot_active:
         bot.stop()
         bot_active = False
-
     return "Bot stopped!"
 
 
-@flask_app.route('/exit_control_panel')
-def exit_control_panel():
+@flask_app.route('/logout')
+@login_required
+def logout_control_panel():
     global bot_active
     if bot_active:
         bot.stop()
         bot_active = False
-    print('Shutting down gracefully...')
-    yield 'Server shutting down...'
-    os.kill(os.getpid(), signal.SIGINT)
+    # yield "Bot stopped!"
+    time.sleep(2)
+    logout_user()
+    return redirect(url_for("login"))
+    # print('Shutting down gracefully...')
+    # yield 'Server shutting down...'
+    # os.kill(os.getpid(), signal.SIGINT)
 
 
 if __name__ == '__main__':
