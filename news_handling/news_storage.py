7ff41674aebb9187e718c932625649aa72900247
CraftBox <i.domnich2017@gmail.com>
2023-11-01 17:33:26 +0200

    - Changes to news_storage.py file.

--- news_handling/news_storage.py
+++ news_handling/news_storage.py
@@ -1,19 +1,35 @@
 import json
+import time
 
 
 class NewsStorage:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, news_file_path="news.json", timestamp_file_path="timestamp.txt"):
+        self.news_file_path = news_file_path
+        self.timestamp_file_path = timestamp_file_path
 
-    def save_news(self, news_data):
-        with open(self.file_path, "w") as json_file:
+    def save_news_and_timestamp_in_seconds(self, news_data: dict):
+        with open(self.news_file_path, "w") as json_file:
             json.dump(news_data, json_file)
+
+        # Save the timestamp to a separate file
+        timestamp = int(time.time())  # Current Unix timestamp
+        with open(self.timestamp_file_path, "w") as timestamp_file:
+            timestamp_file.write(str(timestamp))
 
     def load_news(self):
         try:
-            with open(self.file_path, "r") as json_file:
+            with open(self.news_file_path, "r") as json_file:
                 news_data = json.load(json_file)
             return news_data
-        except FileNotFoundError:
-            # Handle the case where the file doesn't exist or is empty
+        except (FileNotFoundError, json.JSONDecodeError):
+            # TODO: add logging
             return []
+
+    def get_last_download_timestamp_in_seconds(self):
+        try:
+            with open(self.timestamp_file_path, "r") as timestamp_file:
+                timestamp = int(timestamp_file.read())
+            return timestamp
+        except (FileNotFoundError, ValueError):
+            # TODO: add logging
+            return None
