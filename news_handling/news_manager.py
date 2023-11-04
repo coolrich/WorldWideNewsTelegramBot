7b603a2a1e7012c5a74c11bfcf2ecab0d5cb6577
CraftBox <i.domnich2017@gmail.com>
2023-11-04 10:28:49 +0200

    — Integration of the strategy pattern into the news_scraper.py module.

--- news_handling/news_manager.py
+++ news_handling/news_manager.py
@@ -19,7 +19,7 @@ class NewsManager:
         while get_program_state():
             with self.lock:
                 self.logger.debug("In task get_world_news")
-                # a_bot_controller.bot_model.world_news_dict = self.scraper.get_world_news()
+                a_bot_controller.bot_model.world_news_dict = self.scraper.get_world_news()
                 # If there are no files world_news.json and world_news_timestamp.txt, get the world news and timestamp
                 # from the scraper and store them in world_news.json and world_news_timestamp.json respectively.
 
@@ -49,3 +49,22 @@ class NewsManager:
     def __waiting_for_finish_the_program_or_timeout(self, delay, t0):
         while self.program_state_controller.is_program_running() and time.time() - t0 < delay:
             self.lock.wait(delay)
+
+    def get_news(self, a_bot_controller: BotController, delay: int = 60):
+        get_program_state = self.program_state_controller.is_program_running
+        while get_program_state():
+            with self.lock:
+                self.logger.debug("In task get_world_news")
+                a_bot_controller.bot_model.world_news_dict = self.scraper.get_news()
+                # If there are no files world_news.json and world_news_timestamp.txt, get the world news and timestamp
+                # from the scraper and store them in world_news.json and world_news_timestamp.json respectively.
+
+                # Else, get world news from the storage.
+                # Check if the difference between current time and world_news_timestamp.json is more than "delay"
+                # seconds then get news from the scraper
+                self.logger.debug(f"Count of NEWS news: {len(a_bot_controller.bot_model.world_news_dict)}")
+                self.logger.debug(f"Sleeping in get_world_news on {delay}...")
+                self.lock.notify_all()
+                t0 = time.time()
+                self.__waiting_for_finish_the_program_or_timeout(delay, t0)
+        self.logger.debug("End of task get_world_news")
--- news_handling/news_scraper.py
+++ news_handling/news_scraper.py
@@ -1,41 +1,15 @@
 import textwrap
-import sys
 import time
-from selenium import webdriver
-from bs4 import BeautifulSoup
+from abc import ABC, abstractmethod
 from urllib.parse import urljoin
 
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-class NewsScraper:
-    def __init__(self, logger, ua_url=r"https://www.bbc.com/ukrainian", world_url=r"https://www.bbc.com/news"):
-        self.ua_url = ua_url
-        self.world_url = world_url
-        self.logger = logger
 
-    def get_bbc_news_source_from_folder(self):
-        self.logger.debug("Start of get_bbc_news_source_from_folder")
-        with open("test_sources/bbc-news.html", "r", encoding="UTF-8") as f:
-            html_str = f.read()
-        page_source = html_str
-        self.logger.debug("End of get_bbc_news_source_from_folder")
-        return page_source
-
-    def get_bbc_news_ukraine_source_from_folder(self):
-        self.logger.debug("Start of get_bbc_news_ukraine_source_from_folder")
-        try:
-            with open("test_sources/bbc-news-ukraine.html", "r", encoding="utf-8") as f:
-                html_str = f.read()
-            page_source = html_str
-            return page_source
-        except FileNotFoundError:
-            self.logger.error("File not found. Make sure the file 'bbc-news-ukraine.html' exists in the 'test_sources' folder.")
-        except UnicodeDecodeError:
-            self.logger.error("Error decoding the file. Ensure that the file is encoded in UTF-8.")
-        except Exception as e:
-            self.logger.error(f"An unexpected error occurred: {e}")
-        finally:
-            self.logger.debug("End of get_bbc_news_ukraine_source_from_folder")
-        sys.exit()
+class NewsScraperInterface(ABC):
+    def __init__(self, a_logger):
+        self.logger = a_logger
 
     def get_html_source(self, url):
         self.logger.debug("Start of get_html_source")
@@ -47,7 +21,77 @@ class NewsScraper:
         self.logger.debug("End of get_html_source")
         return page_source
 
-    def parse_bbc(self, base_url, html_source):
+    def _get_news_source_from_folder(self, absolute_file_path):
+        self.logger.debug("Start of get_bbc_news_source_from_folder")
+        with open(absolute_file_path, "r", encoding="UTF-8") as f:
+            html_str = f.read()
+        page_source = html_str
+        self.logger.debug(f"End of _get_news_source_from_folder: {absolute_file_path}")
+        return page_source
+
+    @abstractmethod
+    def _parse_news(self, base_url, html_source):
+        pass
+
+    @abstractmethod
+    def get_news(self):
+        pass
+
+    @abstractmethod
+    def get_test_news(self):
+        pass
+
+
+class UANewsScraper(NewsScraperInterface):
+    def __init__(self, a_logger, ua_url=r"https://www.bbc.com/ukrainian"):
+        super().__init__(a_logger)
+        self.ua_url = ua_url
+
+    def _parse_news(self, base_url, html_source):
+        self.logger.debug("Start of parse_bbc_ukraine")
+        base_url = base_url.split('.com')[0] + '.com'
+        bs = BeautifulSoup(html_source, 'html5lib')
+        posts_dict = {}
+        try:
+            section_tags = bs.find('main').find_all('section')
+            for section in section_tags:
+                try:
+                    h3_tag = section.h3
+                    if h3_tag:
+                        heading = h3_tag.text.replace('\n', ' ').title()
+                        href_link = h3_tag.a['href']
+                        full_url = urljoin(base_url.rstrip('/') + '/', href_link.lstrip('/'))
+                        text = textwrap.fill(h3_tag.next_sibling.text.replace('\n', ''), 50)
+                        self.logger.debug(f"Heading: {heading}")
+                        self.logger.debug(f"Url: {full_url}")
+                        self.logger.debug(f"Text: {text}\n")
+                        posts_dict[heading] = [text, full_url]
+                except AttributeError as e:
+                    self.logger.error(f"AttributeError in bbc-ukraine parser: {e}")
+                    continue
+        except Exception as e:
+            self.logger.error(f"An unexpected error in bbc-ukraine occurred: {e}")
+        self.logger.debug("End of parse_bbc_ukraine")
+        return posts_dict
+
+    def get_news(self):
+        bbc_page = self.get_html_source(self.ua_url)
+        news_dict = self._parse_news(self.ua_url, bbc_page)
+        return news_dict
+
+    def get_test_news(self):
+        bbc_page = self._get_news_source_from_folder("test_sources/bbc-news-ukraine.html")
+        news_dict = self._parse_news(self.ua_url, bbc_page)
+        return news_dict
+
+
+class WorldNewsScraper(NewsScraperInterface):
+    def __init__(self, a_logger, world_url=r"https://www.bbc.com/news"):
+        super().__init__(a_logger)
+        self.world_url = world_url
+        self.logger = a_logger
+
+    def _parse_news(self, base_url, html_source):
         self.logger.debug("Start of parse_bbc")
         base_url = base_url.split('.com')[0] + '.com'
         bs = BeautifulSoup(html_source, 'html5lib')
@@ -77,52 +121,14 @@ class NewsScraper:
         self.logger.debug("End of parse_bbc")
         return posts_dict
 
-    def parse_bbc_ukraine(self, base_url, html_source):
-        self.logger.debug("Start of parse_bbc_ukraine")
-        base_url = base_url.split('.com')[0] + '.com'
-        bs = BeautifulSoup(html_source, 'html5lib')
-        posts_dict = {}
-        try:
-            section_tags = bs.find('main').find_all('section')
-            for section in section_tags:
-                try:
-                    h3_tag = section.h3
-                    if h3_tag:
-                        heading = h3_tag.text.replace('\n', ' ').title()
-                        href_link = h3_tag.a['href']
-                        full_url = urljoin(base_url.rstrip('/') + '/', href_link.lstrip('/'))
-                        text = textwrap.fill(h3_tag.next_sibling.text.replace('\n', ''), 50)
-                        self.logger.debug(f"Heading: {heading}")
-                        self.logger.debug(f"Url: {full_url}")
-                        self.logger.debug(f"Text: {text}\n")
-                        posts_dict[heading] = [text, full_url]
-                except AttributeError as e:
-                    self.logger.error(f"AttributeError in bbc-ukraine parser: {e}")
-                    continue
-        except Exception as e:
-            self.logger.error(f"An unexpected error in bbc-ukraine occurred: {e}")
-            # sys.exit()
-        self.logger.debug("End of parse_bbc_ukraine")
-        return posts_dict
-
-    def get_ua_news(self):
-        bbc_page = self.get_html_source(self.ua_url)
-        news_dict = self.parse_bbc_ukraine(self.ua_url, bbc_page)
-        return news_dict
-
-    def get_world_news(self):
+    def get_news(self):
         bbc_page = self.get_html_source(self.world_url)
-        news_dict = self.parse_bbc(self.world_url, bbc_page)
+        news_dict = self._parse_news(self.world_url, bbc_page)
         return news_dict
 
-    def get_test_ua_news(self):
-        bbc_page = self.get_bbc_news_ukraine_source_from_folder()
-        news_dict = self.parse_bbc_ukraine(self.ua_url, bbc_page)
-        return news_dict
-
-    def get_test_world_news(self):
-        bbc_page = self.get_bbc_news_source_from_folder()
-        news_dict = self.parse_bbc(self.world_url, bbc_page)
+    def get_test_news(self):
+        bbc_page = self._get_news_source_from_folder("test_sources/bbc-news.html")
+        news_dict = self._parse_news(self.world_url, bbc_page)
         return news_dict
 
 
@@ -138,9 +144,9 @@ if __name__ == '__main__':
     )
     logger = logging.getLogger()
     logger.debug("Start of the __main__() method in NewsScraper class")
-    ns = NewsScraper(logger=logger)
+    ns = UANewsScraper(a_logger=logger)
     # world_news = ns.get_world_news()
-    ua_news = ns.get_ua_news()
+    ua_news = ns.get_news()
     # logger.debug("World news:")
     # logger.debug(f"Count of world news: {len(world_news)}")
     logger.debug("UA news:")
