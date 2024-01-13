7c58868c4b7f6f7f4e853bc6661bccc3bc29a40a
CraftBox <i.domnich2017@gmail.com>
2024-01-13 12:24:06 +0200

    — Files are loaded from another project — Some files are added to .gitignore

--- core/keyboard_button_names.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class KeyboardButtonsNames(Enum):
-    UA = 'Новини України'
-    WORLD = 'Новини Світу'
--- news_handling/loader_interface.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-from country_codes.country_codes import CountryCodes
-from news_handling.news_article import NewsArticle
-
-
-class LoaderInterface:
-    def load_news(self) -> (CountryCodes, List[NewsArticle]):
-        pass
