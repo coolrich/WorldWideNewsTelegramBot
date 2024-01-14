565ff04dd7bd7ae21b14648d7c1d8f72b191bfe0
CraftBox <i.domnich2017@gmail.com>
2024-01-14 17:15:59 +0200

    — Changes in main.py: checking for GCP triggers.

--- user_storage.py
+++ user_storage.py
@@ -7,7 +7,17 @@ class Users:
     def __init__(self, bucket_name: str = "users_news_data"):
         self.bucket_name = bucket_name
         self.storage_client = storage.Client()
-        self.bucket = self.storage_client.bucket(bucket_name)
+        self.bucket: storage.Bucket = self.storage_client.bucket(bucket_name)
+        if not Users.__is_bucket_exists(bucket_name):
+            self.bucket = self.storage_client.create_bucket(bucket_name)
+
+    @staticmethod
+    def __is_bucket_exists(bucket_name):
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        if bucket.exists():
+            return True
+        return False
 
     def add_user(self, user: User):
         """Зберігає об'єкт User у Cloud Storage."""
