e6b9be88d3aa2e98b4133fbe0b2c24e9fa481199
coolrich <i.domnich2017@gmail.com>
2024-01-15 18:47:22 +0200

    Small changes.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import google.cloud.secretmanager
 import bot_controller
 
 
-#################################
 def run_bot(token: str, request_json):
     data = request_json
     if 'message' in data:
@@ -12,9 +11,6 @@ def run_bot(token: str, request_json):
         bot_controller.BotController(token).handle_message(message)
         return True
     return False
-
-
-#####################################################################################
 
 def get_secret(secret_id, project_id="worldwidenewstelegrambotdev", version_id="latest"):
     # Створіть клієнт менеджера секретів.
@@ -25,8 +21,6 @@ def get_secret(secret_id, project_id="worldwidenewstelegrambotdev", version_id="
     payload = response.payload.data.decode("UTF-8")
     return payload
 
-
-####################################################################################
 @functions_framework.http
 def handle_request(request):
     secret_id = "test_bot_token"
@@ -34,4 +28,3 @@ def handle_request(request):
     request_json = request.get_json(silent=True)
     run_bot(secret, request_json)
     return f'Request: {request}\nRequest proccessing is finished!'
-######
